bb1b09d1914af006b2eeb3ebc307c92ec964da8c
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-03-22 00:58:11 -0400

    syntax updates

--- invite.js
+++ invite.js
@@ -1,1 +1,6 @@
-var inputs = document.getElementsByClassName('uiButton _1sm'); for(var i=0; i<inputs.length;i++) { inputs[i].click(); }
+
+var inputs = document.getElementsByClassName('uiButton _1sm'); 
+
+for(var i=0; i<inputs.length;i++) { 
+    inputs[i].click(); 
+}
